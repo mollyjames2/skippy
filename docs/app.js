9d6a14a57124e8070c5fac4fc69602482abbac61
Molly James <moja@pml.ac.uk>
2026-01-16 19:14:18 +0000

    Fix app.js string quoting and add location change toast

--- docs/app.js
+++ docs/app.js
@@ -48,15 +48,15 @@ function showToast(message) {
 
   document.body.appendChild(el);
 
-  setTimeout(function() {
+  setTimeout(function () {
     el.style.opacity = "1";
   }, 10);
 
-  setTimeout(function() {
+  setTimeout(function () {
     el.style.opacity = "0";
   }, 2500);
 
-  setTimeout(function() {
+  setTimeout(function () {
     if (el && el.parentNode) el.parentNode.removeChild(el);
   }, 2750);
 }
@@ -77,7 +77,7 @@ function requireLocationOrRedirect() {
   return {
     slug: slug,
     name: localStorage.getItem("skippy_locationName") || "South West UK",
-    group: localStorage.getItem("skippy_locationGroup") || ""
+    group: localStorage.getItem("skippy_locationGroup") || "",
   };
 }
 
@@ -89,22 +89,28 @@ async function fetchWeek(apiBase) {
 }
 
 function renderWeek(data, loc) {
-  setText("location", (loc && loc.name) ? loc.name : (data.location || "South West UK"));
+  setText(
+    "location",
+    (loc && loc.name) ? loc.name : (data.location || "South West UK"),
+  );
 
   var best = data.best_day || null;
   if (best) {
     setHtml(
       "bestCard",
-      ""
-        + '<div class="muted small">Best day this week</div>'
-        + '<div class="spacer"></div>'
-        + '<div class="row">'
-        + '  <div>'
-        + '    <div style="font-weight:800; font-size:18px;">' + best.dow + " " + best.label + "</div>"
-        + '    <div class="muted small">Best window: ' + best.best_time.start + " - " + best.best_time.end + "</div>"
-        + "  </div>"
-        + '  <div class="' + pillClass(best.score) + '" style="font-size:16px;">' + best.score + "</div>"
-        + "</div>"
+      "" +
+        '<div class="muted small">Best day this week</div>' +
+        '<div class="spacer"></div>' +
+        '<div class="row">' +
+        "  <div>" +
+        '    <div style="font-weight:800; font-size:18px;">' + best.dow + " " +
+        best.label + "</div>" +
+        '    <div class="muted small">Best window: ' + best.best_time.start +
+        " - " + best.best_time.end + "</div>" +
+        "  </div>" +
+        '  <div class="' + pillClass(best.score) +
+        '" style="font-size:16px;">' + best.score + "</div>" +
+        "</div>",
     );
   } else {
     setHtml("bestCard", '<div class="muted small">No data</div>');
@@ -116,32 +122,32 @@ function renderWeek(data, loc) {
   daysEl.innerHTML = "";
 
   var days = data.days || [];
-  days.forEach(function(d) {
+  days.forEach(function (d) {
     var href = "./day.html?date=" + encodeURIComponent(d.date);
 
     var card = document.createElement("a");
     card.className = "card";
     card.href = href;
 
-    card.innerHTML =
-      ""
-        + '<div class="row">'
-        + "  <div>"
-        + '    <div style="font-weight:800;">' + d.dow + "</div>"
-        + '    <div class="muted small">' + d.condition + "</div>"
-        + "  </div>"
-        + '  <div style="text-align:right;">'
-        + '    <div style="font-weight:800;">' + d.temp_c + "&deg;C</div>"
-        + '    <div class="' + pillClass(d.score) + '">' + d.rating + "</div>"
-        + "  </div>"
-        + "</div>"
-        + '<div class="spacer"></div>'
-        + '<div class="row small muted">'
-        + "  <div>Wind: " + d.wind.kts + " kts " + d.wind.dir + "</div>"
-        + "  <div>Waves: " + d.waves.m + " m</div>"
-        + "</div>"
-        + '<div class="spacer"></div>'
-        + '<div class="muted small">Best time to boat: <b>' + d.best_time.start + " - " + d.best_time.end + "</b></div>';
+    card.innerHTML = "" +
+      '<div class="row">' +
+      "  <div>" +
+      '    <div style="font-weight:800;">' + d.dow + "</div>" +
+      '    <div class="muted small">' + d.condition + "</div>" +
+      "  </div>" +
+      '  <div style="text-align:right;">' +
+      '    <div style="font-weight:800;">' + d.temp_c + "&deg;C</div>" +
+      '    <div class="' + pillClass(d.score) + '">' + d.rating + "</div>" +
+      "  </div>" +
+      "</div>" +
+      '<div class="spacer"></div>' +
+      '<div class="row small muted">' +
+      "  <div>Wind: " + d.wind.kts + " kts " + d.wind.dir + "</div>" +
+      "  <div>Waves: " + d.waves.m + " m</div>" +
+      "</div>" +
+      '<div class="spacer"></div>' +
+      '<div class="muted small">Best time to boat: <b>' + d.best_time.start +
+      " - " + d.best_time.end + "</b></div>";
 
     daysEl.appendChild(card);
   });
